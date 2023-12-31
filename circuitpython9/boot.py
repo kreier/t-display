b62709e8ce4f08a6bf9a683d62c9dc49d970683b
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-12-31 20:37:41 +0700

    enable write to the local file system

--- circuitpython9/boot.py
+++ circuitpython9/boot.py
@@ -3,6 +3,7 @@
 # 2023/02/23 v0.2
 # 2023/12/29 v0.3 adjusted for CircuitPython 9.0 with fourwire
 
+from urllib.request import HTTPDigestAuthHandler
 import board, busio, fourwire
 from adafruit_st7789 import ST7789
 
@@ -16,6 +17,29 @@ if board.board_id == 'vcc_gnd_yd_rp2040' or board.board_id == 'raspberry_pi_pico
         spi, command=tft_dc, chip_select=tft_cs, reset=board.GP12
     )
     display = ST7789(display_bus, width=240, height=240, rowstart=80, rotation=0)
+
+    import digitalio, storage, time
+
+    button = digitalio.DigitalInOut(board.GP3)
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+    led = digitalio.DigitalInOut(board.LED)
+    led.direction = digitalio.Direction.OUTPUT
+    led.value = True
+
+    timer = 6
+    print(f"Press the button in the next {timer} seconds to activate saving")
+    end = time.monotonic() + timer
+    while end - time.monotonic() > 0:
+        if not button.value:
+            print("write access activated")
+            storage.remount("/", False)
+            led.value = False
+        if end - timer + 1 < time.monotonic():
+            timer -= 1
+            print(f"{timer} seconds left | " + "*" * timer)
+    print("Not activated")
+
 if board.board_id == 'lilygo_ttgo_tdisplay_esp32_4m':
     tft_dc    = board.LCD_DC
     tft_cs    = board.LCD_CS
